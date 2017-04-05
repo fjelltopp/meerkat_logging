84ef049f35dc46ec6ae34b72465ed5df6a5c41b8
jyri <Jyri Soppela>
2017-04-05 10:37:46 +0100

    database model

---
+++ meerkat_logging/model.py
@@ -0,0 +1,22 @@
+"""
+Logging database model definition
+"""
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import validates
+from sqlalchemy.event import listen
+
+Base = declarative_base()
+
+class Log(Base):
+    __tablename__ = 'log'
+    
+    uuid = Column(String, primary_key=True)
+    timestamp = Column(DateTime, index=True)
+    type = Column(String)
+    source = Column(String, index=True)
+    source_type = Column(String)
+    implementation = Column(String)
+    event_data = Column(JSONB)
+
